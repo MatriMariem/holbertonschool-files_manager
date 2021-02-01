1181cd4f6f7216f51df7ddc48ad7101686762648
MatriMariem <meriemmatri1994@gmail.com>
2021-02-01 15:33:26 +0100

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+const redis = require("redis");
+class RedisClient {
+  constructor () {
+this.client = redis.createClient();
+this.client.on("error", function(error) {
+  console.error(error);
+});
+  }
+  isAlive () {
+    return this.client.connected;
+  }
+  async get(key) {
+    return await this.client.get(key, (err, val) => {
+      if (err) { throw err }
+        return(val);
+    })
+  }
+async set(key, val, dur) {
+  this.client.set(key, val);
+  this.client.expire(key, dur);
+}
+async del(key) {
+  this.client.del(key, (err) => {
+    if (err) { throw err }
+  })
+}
+
+
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
